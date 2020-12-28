f7d1f3d9e510208516e2efd86ff3d6bee3a85de6
Anatolii <tolikdemchuk507@gmail.com>
2020-12-28 13:20:53 +0200

    Build 0.2

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.sql import select
 import jwt
 
 from models import Base, UserInDB, Salt
@@ -20,11 +22,17 @@ PASSWORD = 'f36ffdd60bcbac1b42d18d652dc465dbda47859d46fc9893db8229b1cdae67dd'  #
 HOST = 'ec2-54-75-229-28.eu-west-1.compute.amazonaws.com'  # environ.get('HOST')
 PORT = '5432'  # environ.get('PORT')
 DATABASE = 'd1p21lku7v294h'  # environ.get('DATABASE')
+SECRET_KEY = 'dester123456789dester'  # environ.get('SECRET_KEY')
+ALGORITHM = 'HS256'  # environ.get('ALGORITHM')
 
 
 class UserReg(BaseModel):
     username: str
     password: str
+
+
+class UserValidation(BaseModel):
+    username: str
 
 
 async def async_return_engine():
@@ -47,3 +55,48 @@ async def user_register(user: UserReg, engine: AsyncEngine = Depends(async_retur
     except IntegrityError:
         raise HTTPException(status_code=409,
                             detail="User with this username already exist.")
+
+
+async def login_user(username, password):
+    engine = await async_return_engine()
+    async with AsyncSession(engine) as session:
+        stmt = select(UserInDB).where(UserInDB.username == username)
+        result_user = await session.execute(stmt)
+        if result_user.first():
+            stmt = select(Salt).where(Salt.user == username)
+            result_salt = await session.execute(stmt)
+            hash_new_password = check_password_hash(password, result_salt.first().salt)
+            if hash_new_password != result_user.first().password:
+                return False
+        else:
+            return False
+    return True
+
+
+def create_token(*, data: dict):
+    to_encode = data.copy()
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def decode_token(token):
+    error_detail = HTTPException(
+        status_code=401,
+        detail="Bad data",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("user")
+        if username is None:
+            raise error_detail
+        valid_user = UserValidation(username=username)
+    except jwt.PyJWTError:
+        raise error_detail
+    engine = await async_return_engine()
+    async with AsyncSession(engine) as session:
+        stmt = select(UserInDB).where(UserInDB.username == valid_user.username)
+        result = await session.execute(stmt)
+        if result.first() is None:
+            raise error_detail
+        return result.first()
